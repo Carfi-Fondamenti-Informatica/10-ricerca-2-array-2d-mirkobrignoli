586bf42b082dc2d906fa3ec8acbc7a9fd663c426
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-06-08 21:10:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char lista[10][20];
+    char a[20];
+    int pos = 0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            a[j] = ' ';
+            lista[i][j] = ' ';
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        cin >> lista[i];
+    }
+
+    cin >> a;
+    if(cerca(lista,pos,a)){
+        cout << "trovato in posizione "<< pos;
+    }
+  
+    else{
+        cout << "non trovato";
+    }
+
+    return 0;
 }
