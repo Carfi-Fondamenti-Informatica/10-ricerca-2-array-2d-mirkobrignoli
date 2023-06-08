288a037aebf43f698ddb46227c6d034898b45405
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-06-08 21:01:37 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+#include "lib.h"
 
+bool cerca(char listanome[10][20], int &pos, char a[]){
+    for(int i=0;i<10;i++){
+
+        for(int j=0;j<20;j++){
+            if(listanome[i][j] != a[j]){
+                  j = 20;
+            }else{
+
+                    if(j==19){
+                          pos = (i)/2;
+
+                           return true;
+
+                    }}}
+    }
+    return false;
+}
